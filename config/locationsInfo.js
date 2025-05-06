820b181382ea4daed47e632dc807d17e1e88870e
arsahak <arsahak99@gmail.com>
2025-05-06 13:55:43 +0600

    update locations info

---
+++ config/locationsInfo.js
@@ -0,0 +1,50 @@
+export const locationsInfo = [
+  {
+    id: "brandon_clinic",
+    title: "BRANDON CLINIC",
+    address: "1216 Oakfield Dr Brandon, FL 33511",
+    phone: "(813) 868-4377",
+    availableSchedule: [
+      { day: "Today", time: ["8:30 am-7:00 pm"] },
+      { day: "Tuesday", time: ["8:30 am - 1:00 pm"] },
+      { day: "Thursday", time: ["8:30 am-7:00 pm"] },
+    ],
+  },
+
+  {
+    id: "clearwater_clinic",
+    title: "CLEARWATER CLINIC",
+    address: "901 N Hercules Ave Clearwater, FL 33765",
+    phone: "(727) 442-5569",
+    availableSchedule: [
+      { day: "Monday", time: ["8:30 am-7:00 pm"] },
+      { day: "Tuesday", time: ["8:30 am-7:00 pm"] },
+      { day: "Thursday", time: ["8:30 am-7:00 pm"] },
+      { day: "Saturday", time: ["8:30 am-1:00 pm"] },
+    ],
+  },
+
+  {
+    id: "lakeland_clinic",
+    title: "LAKELAND CLINIC",
+    address: "1519 Lakeland Hills Blvd Lakeland, FL 33805",
+    phone: "(863) 688-3388",
+    availableSchedule: [
+      { day: "Monday", time: ["8:30 am-7:00 pm"] },
+      { day: "Wednesday", time: ["8:30 am-7:00 pm"] },
+      { day: "Friday", time: ["8:30 am-7:00 pm"] },
+    ],
+  },
+
+  {
+    id: "ruskin_clinic",
+    title: "RUSKIN CLINIC",
+    address: "Ruskin, Florida 33570",
+    phone: "(813) 645-4480",
+    availableSchedule: [
+      { day: "Monday", time: ["8:30 am-1:00 pm", "2:30 pm-7:00 pm"] },
+      { day: "Wednesday", time: ["8:30 am-1:00 pm", "2:30 pm-7:00 pm"] },
+      { day: "Friday", time: ["8:30 am-1:00 pm", "2:30 pm-7:00 pm"] },
+    ],
+  },
+];
